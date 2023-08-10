cb4360f2bce25880a89a12cc5bdcefeeb70d372c
SusanaCor <scorderoa@fwdcostarica.com>
2023-08-10 13:15:29 -0600

    Practica lista

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const JIJIJI = () =>{
-    return "fuck you"
-}
+const listNumeros =  [23, 24, 25, 27,28];
+
+const lisTotal = listNumeros.reduce((suma , numeros) => suma + numeros, 0);
+
+console.log("La suma total es:", lisTotal);
